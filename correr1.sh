dc4603524cf586d4b777de8f160d55eb970f8435
ernesto <eszini@gmail.com>
2024-05-02 11:02:04 +0100

    limpieza

--- correr1.sh
+++ correr1.sh
@@ -1,13 +1,13 @@
 
 
 # la primera corrida, genero file igual al input
-./cnv_f07 -v -f -opcion=d5 -inp=programa1.for -out=p2.for  -aux=p2.lo1 > log1
+./tfor -v -f -opcion=d5 -inp=programa1.for -out=programa2.for -aux=parser.log > log1
 
-
-# la segunda, dejo los tokens a ver como separa el progama
-./cnv_f07 -v  -opcion=d5 -inp=programa1.for -out=p2.tkn  -aux=p2.lo2 > log2
 
 
 # miro si el parser encontro caracteres que no tiene en la tabla
-cat p2.lo2
+cat parser.log
 
+
+# check basico
+wc programa1.for programa2.for
--- correr2.sh
+++ correr2.sh
@@ -1,7 +1,14 @@
 
-
+# todavia en construccion
+#
 # ejecuto proceso aparte proc1 - cargo todo el archivo en un vector
-./cnv_f07 -v -f -opciones=d5 -inp=programa1.for -out=p2.for -proc=1 -src=programa1.for -aux=p2.log > log
+
+./tfor -v -f -opciones=d5 -proc=1 -src=programa1.for  -aux=parser.log > log2
+
+cat parser.log
+
+
+
 
 
 
